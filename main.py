03eec35c5f8aa9f1ce4d7eb53b5d98357a52cffe
Aaron Carson <aaron@aaroncarson.co.uk>
2021-02-03 01:18:33 +0000

    DONE: ✅ Downloads current followers from Twitter ✅ Creates a dictionary containing a timestamp, current followers, length of follower list, new unfollowers and new followers
    TODO:
    ⌛️ Download the last row from BigQuery
    ⌛️ Compare the last follower list with the latest one
    ⌛️ Generate a list of new unfollowers and new unfollowers
    ⌛️ Some kind of notification system

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import tweepy
 import json
+from google.cloud import bigquery
+import os
+import datetime
 
-## set up auth
+## location of BigQuery service account creds
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/aaroncarson/src/github.com/caraar12345/twitter-unfollowers/googleauth.json'
+## sets up BQ access
+bqClient = bigquery.Client()
+bqFollowersTable = "twitter_analysis.followers"
+
+
+## set up Twitter auth
 with open('auth.json') as authJSONFile:
     authKeys = json.load(authJSONFile)
 
 auth = tweepy.OAuthHandler(authKeys['consumer_key'],authKeys['consumer_secret'])
 auth.set_access_token(authKeys['access_token'],authKeys['access_secret'])
-
 api = tweepy.API(auth)
-
 authedUser = api.me().screen_name
-print(authedUser)
+#print(authedUser)
+
+currentFollowerList = api.followers_ids(authedUser)
+
+newUnfollowers = []
+newFollowers = []
+
+currentFollowers = [{
+        "timestamp": datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S"),
+        "account_ids": str(currentFollowerList),
+        "length": len(currentFollowerList),
+        "unfollowers": str(newUnfollowers),
+        "new_followers": str(newFollowers)
+        }]
+
+#print(currentFollowers)
+
+bqJSON = [currentFollowers]
+
+bqClient.insert_rows_json(bqFollowersTable, currentFollowers)
